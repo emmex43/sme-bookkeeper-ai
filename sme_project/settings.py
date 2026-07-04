067524fa59bf0cf4574b4c5fc030daf21b045ce5
Akhabue Ehime <emmexanimation@gmail.comm>
2026-07-04 13:31:53 +0100

    connect supabase PostgreSQL database

--- sme_project/settings.py
+++ sme_project/settings.py
@@ -83,19 +83,14 @@ WSGI_APPLICATION = 'sme_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
+    'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True,
     )
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
